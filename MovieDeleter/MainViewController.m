482c778716876cec6bea58ecf4bfe609769a81a6
Christopher Loessl <cloessl@x-berg.de>
2013-06-29 15:03:49 +0200

    This shows all movies on the device
    iPhone and iPad

--- MovieDeleter/MainViewController.m
+++ MovieDeleter/MainViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "MainViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface MainViewController ()
-
+@interface MainViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIPopoverControllerDelegate>
+@property (weak, nonatomic) UIActionSheet *sinkControlActionSheet;  // so we won't get multiple popovers
+@property (strong, nonatomic) UIPopoverController *imagePickerPopover;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *showMoviesButton;
 @end
 
 @implementation MainViewController
@@ -25,5 +28,39 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self showMoviesFromSender:self.showMoviesButton];
+}
+
+- (IBAction)showMoviesFromSender:(UIBarButtonItem *)sender {
+    UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    //    NSString *fileType = (NSString *)kUTTypeImage;
+    NSString *fileType = (NSString *)kUTTypeMovie;
+    if (!self.imagePickerPopover && [UIImagePickerController isSourceTypeAvailable:sourceType]) {
+        NSArray *availableMediaTypes = [UIImagePickerController availableMediaTypesForSourceType:sourceType];
+        if ([availableMediaTypes containsObject:fileType]) {
+            UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+            picker.sourceType = sourceType;
+            picker.mediaTypes = @[fileType];
+            picker.allowsEditing = YES;
+            picker.delegate = self;
+            if ((sourceType != UIImagePickerControllerSourceTypeCamera) && (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)) {
+                self.imagePickerPopover = [[UIPopoverController alloc] initWithContentViewController:picker];
+                [self.imagePickerPopover presentPopoverFromBarButtonItem:sender permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+                self.imagePickerPopover.delegate = self;
+            } else {
+                [self presentViewController:picker animated:YES completion:nil];
+            }
+        }
+    }
+}
+
+- (void)popoverControllerDidDismissPopover:(UIPopoverController *)popoverController
+{
+    self.imagePickerPopover = nil;
+}
+
 
 @end
